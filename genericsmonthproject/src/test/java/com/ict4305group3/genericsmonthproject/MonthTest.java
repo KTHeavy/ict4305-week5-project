582c91593a760f58d34ade62717d7f38c0766a8d
eyevanovich <54329731+eyevanovich@users.noreply.github.com>
2020-07-14 08:40:16 -0700

    Add month test class

---
+++ genericsmonthproject/src/test/java/com/ict4305group3/genericsmonthproject/MonthTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ict4305group3.genericsmonthproject;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author ipiesh
+ */
+public class MonthTest {
+    
+    public MonthTest() {
+    }
+
+    /**
+     * Test of add method, of class Month.
+     */
+    @Test
+    public void testAdd() {
+    }
+
+    /**
+     * Test of get method, of class Month.
+     */
+    @Test
+    public void testGet() {
+    }
+    
+}
